553dc8d87f71b1a2d373ed33c90527c4e59259d5
Ashish Karhade <akarhade5@gmail.com>
2022-08-13 21:54:16 +0530

    database mongodb conneciton established

--- backend/routes/connection.go
+++ backend/routes/connection.go
@@ -1,1 +1,37 @@
 package routes
+
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"time"
+)
+
+func DBInstance() *mongo.Client {
+	mongoDB := "mongodb://localhost:27017/caloriesdb"
+
+	client, err := mongo.NewClient(options.Client().ApplyURI(mongoDB))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
+
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to MONGODB")
+	return client
+}
+
+var Client *mongo.Client = DBInstance()
+
+func OpenCollection(client *mongo.Client, collectionName string) (*mongo.Collection) {
+	var collection *mongo.Collection = client.Database("caloriesdb").Collection(collectionName)
+	return collection
+}
--- backend/routes/entries.go
+++ backend/routes/entries.go
@@ -11,14 +11,35 @@ import (
 	"time"
 )
 
-var entryCollection *mongo.Collection = openCollection(Client, "calories")
+var entryCollection *mongo.Collection = OpenCollection(Client, "calories")
 
 func AddEntry(c *gin.Context){
 
 }
 
 func GetEntries(c *gin.Context){
+	var ctx, cancel = context.WithTimeout(context.Background(), 100 * time.Second)
 
+	var entries []bson.M
+
+	cursor, err := entryCollection.Find(ctx, bson.M{}) //bson.M{} - empty gets all documents
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		fmt.Println(err)
+		return
+	}
+
+	if err = cursor.All(ctx, &entries); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		fmt.Println(err)
+		return
+	}
+
+	defer cancel()
+
+	fmt.Println(entries)
+	c.JSON(http.StatusOK, entries)
 }
 
 func GetEntryById(c *gin.Context){
@@ -48,6 +69,7 @@ func DeleteEntry(c *gin.Context){
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		fmt.Println(err)
+		return
 	}
 
 	defer cancel()
